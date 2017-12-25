088e957c5b01647cfcd1d3487f52100bbf1ef086
lukinu <Nosoup4u>
2017-12-25 22:18:25 +0500

    solved

--- Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/androidTest/java/com/example/android/todolist/data/TestTaskContentProvider.java
+++ Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/androidTest/java/com/example/android/todolist/data/TestTaskContentProvider.java
@@ -24,10 +24,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.android.todolist.data.TaskContentProvider;
-import com.example.android.todolist.data.TaskContract;
-import com.example.android.todolist.data.TaskDbHelper;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
